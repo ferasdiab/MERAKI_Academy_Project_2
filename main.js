17174f9499e65ae2d07bb754066f896daaf1218b
ferasdiab <dfiras96@gmail.com>
2021-04-28 15:09:10 +0300

    ambrove rating to have value dibinding on number of user

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ const ActionMovieName = ["Nobody", "The Equalizer","The Dark Knight","John Wick"
 const ActionMovieBref = [ "A bystander who intervenes to help a woman being harassed by a group of men becomes the target of a vengeful drug lrd.", " A man who believes he has put his mysterious past behind him cannot stand idly by when he meets a young girl under the control of ultra-violent Russian gangsters." ,"When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.","An ex-hit-man comes out of retirement to track down the gangsters that killed his dog and took everything from him."]
 const ActionMovieImj = ["https://m.media-amazon.com/images/M/MV5BMjM5YTRlZmUtZGVmYi00ZjE2LWIyNzAtOWVhMDk1MDdkYzhjXkEyXkFqcGdeQXVyMjMxOTE0ODA@._V1_UX182_CR0,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BMTQ2MzE2NTk0NF5BMl5BanBnXkFtZTgwOTM3NTk1MjE@._V1_UX182_CR0,0,182,268_AL_.jpg" ,"https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_UX182_CR0,0,182,268_AL_.jpg"]
 const ActionMovieYear = [ "26 March 2021","26 September 2014", "18 July 2008","24 October 2014"]
-const ActionMovieRating = ["5","9","8","7"]
+const ActionMovieRating = [{rating :10 ,userNum:1},{rating :6 ,userNum:50},{rating :7 ,userNum:50}]
 const ActionMovieCast = [{Director:" Ilya Naishuller" ,Writer :"Derek Kolstad", Stars:"Bob Odenkirk, Aleksey Serebryakov, Connie Nielsen" },
 {Director:"Antoine Fuqua" ,Writer :"Richard Wenk, Michael Sloan", Stars:"Denzel Washington, Marton Csokas, Chloë Grace Moretz"},
 {Director:"Christopher Nolan" ,Writer :" Jonathan Nolan (screenplay), Christopher Nolan (screenplay)", Stars:"Christian Bale, Heath Ledger, Aaron Eckhart "},
@@ -50,9 +50,8 @@ for (let i=0; i<ActionMovieName.length;i++){
     $("#ActionMovie"+i).append(`<div class=movieDivCH2 id='actionCH${i}' ></div>`)
     $("#actionCH"+i).append(`<h1>${ActionMovieName[i]}</h1>`)
     $("#actionCH"+i).append(`<h2>${ActionMovieYear[i]}</h2>`)
-
-    localStorage.setItem(ActionMovieName[i],ActionMovieRating[i])
-    $("#actionCH"+i).append(`<h4>Rating : ${localStorage.getItem(ActionMovieName[i])} From 10</h4>`)
+    localStorage.setItem(ActionMovieName[i],ActionMovieRating[i].rating)
+    $("#actionCH"+i).append(`<h4>Rating : ${Math.round(ActionMovieRating[i].rating)} From ${ActionMovieRating[i].userNum}`)
     $("#actionCH"+i).append(`<div class='rating'><button id='actionRating${i}'>click to rate</button></div>`)
 
 
@@ -93,10 +92,16 @@ for (let i=0; i<ActionMovieName.length;i++){
 }
 
 const fun1 = (i)=>{
+    if ( parseInt($(`#rateThis${i}`).val())  <=10  ){
     $("#addRate"+i).remove()
-    localStorage.setItem(ActionMovieName[i],$(`#rateThis${i}`).val())
-    ActionMovieRating[i] = $(`#rateThis${i}`).val()
+    let rate = ActionMovieRating[i].rating*ActionMovieRating[i].userNum
+    ActionMovieRating[i].userNum += 1
+    let newRate = (rate + parseInt($(`#rateThis${i}`).val())) / ActionMovieRating[i].userNum
+    ActionMovieRating[i].rating = newRate 
+    ActionMovieRating[i].userNum = ActionMovieRating[i].userNum
+
     rinderaction()
+}
 
 }
 
@@ -140,7 +145,7 @@ const comedyMovieName =["The Hitman's Bodyguard ","Step Brothers","Deadpool"]
 const comedyMovieBref = ["The world's top bodyguard gets a new client, a hitman who must testify at the International Criminal Court. They must put their differences aside and work together to make it to the trial on time.","Two aimless middle-aged losers still living at home are forced against their will to become roommates when their parents marry.","A wisecracking mercenary gets experimented on and becomes immortal but ugly, and sets out to track down the man who ruined his looks."]
 const comedyMovieImj =["https://m.media-amazon.com/images/M/MV5BMjQ5NjA2NDg1MV5BMl5BanBnXkFtZTgwMDAzNDc4MjI@._V1_UX182_CR0,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BODViZDg3ZjYtMzhiYS00YTVkLTk4MzktYWUxMTlkYjc1NjdlXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BYzE5MjY1ZDgtMTkyNC00MTMyLThhMjAtZGI5OTE1NzFlZGJjXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"]
 const comedyMovieYear = ["18 August 2017"," 25 July 2008"," 12 February 2016"]
-const comedyMovieRating = ["5","9","8","7"]
+const comedyMovieRating = [{rating :10 ,userNum:1},{rating :6 ,userNum:50},{rating :7 ,userNum:50}]
 const comedyMovieCast =[{Director:"Patrick Hughes",Writer:"Tom O'Connor",Stars:" Ryan Reynolds, Samuel L. Jackson, Gary Oldman "},
     {Director:"Adam McKay",Writer:" Will Ferrell, Adam McKay",Stars:"Will Ferrell, John C. Reilly, Mary Steenburgen "},
     {Director:"Tim Miller",Writer:"Rhett Reese, Paul Wernick",Stars:" Ryan Reynolds, Morena Baccarin, T.J. Miller "}]
@@ -158,8 +163,8 @@ const rinderComedy = ()=>{
         $("#ComedyCH"+i).append(`<h1>${comedyMovieName[i]}</h1>`)
         $("#ComedyCH"+i).append(`<h2>${comedyMovieYear[i]}</h2>`)
 
-        localStorage.setItem(comedyMovieName[i],comedyMovieRating[i])
-        $("#ComedyCH"+i).append(`<h4>Rating : ${localStorage.getItem(comedyMovieName[i])} From 10</h4>`)
+        localStorage.setItem(comedyMovieName[i],comedyMovieRating[i].rating)
+        $("#ComedyCH"+i).append(`<h4>Rating : ${Math.round(localStorage.getItem(comedyMovieName[i]))}/10 From ${comedyMovieRating[i].userNum} Rate </h4>`)
         $("#ComedyCH"+i).append(`<div class='rating'><button id='comedyRating${i}'>click to rate</button></div>`)
 
         $("#ComedyCH"+i).append(`<p id='breafcomedy${i}'>${comedyMovieBref[i]}</p>`)
@@ -182,7 +187,7 @@ const rinderComedy = ()=>{
             $("#comedyRating"+i).remove()
             $("#breafcomedy"+i).remove()
             $("#Comedymore"+i).remove()
-            $("#ComedyCH"+i).append(`<input id='rateThisA${i}' type='text' placeholder='your Rate'><br>`)
+            $("#ComedyCH"+i).append(`<input id='rateThisA${i}' type='number' placeholder='your Rate(from 0 to 10)'><br>`)
             $("#ComedyCH"+i).append(`<button onclick='fun1A(${i})' id='addRateA${i}'>Add your Rate</button>`)
             
     
@@ -195,9 +200,15 @@ const rinderComedy = ()=>{
     }
 
     const fun1A = (i)=>{
+        if ( parseInt($(`#rateThisA${i}`).val())  <=10  ){
         $("#addRateA"+i).remove()
-        localStorage.setItem(comedyMovieName[i],$(`#rateThisA${i}`).val())
-        comedyMovieRating[i] = $(`#rateThisA${i}`).val()
+        let rate = comedyMovieRating[i].rating*comedyMovieRating[i].userNum
+        comedyMovieRating[i].userNum += 1
+        let newRate = (rate + parseInt($(`#rateThisA${i}`).val())) / comedyMovieRating[i].userNum
+        comedyMovieRating[i].rating = newRate 
+        comedyMovieRating[i].userNum = comedyMovieRating[i].userNum
+
         rinderComedy()
+        }
     
     }
