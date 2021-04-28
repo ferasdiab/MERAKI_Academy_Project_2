ddbb66835f1bc58de20a538f2b4a67a6cd9508f9
ferasdiab <dfiras96@gmail.com>
2021-04-28 10:55:36 +0300

    add rating related with the localstorage in action list

--- main.js
+++ main.js
@@ -33,6 +33,7 @@ const ActionMovieName = ["Nobody", "The Equalizer","The Dark Knight","John Wick"
 const ActionMovieBref = [ "A bystander who intervenes to help a woman being harassed by a group of men becomes the target of a vengeful drug lrd.", " A man who believes he has put his mysterious past behind him cannot stand idly by when he meets a young girl under the control of ultra-violent Russian gangsters." ,"When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.","An ex-hit-man comes out of retirement to track down the gangsters that killed his dog and took everything from him."]
 const ActionMovieImj = ["https://m.media-amazon.com/images/M/MV5BMjM5YTRlZmUtZGVmYi00ZjE2LWIyNzAtOWVhMDk1MDdkYzhjXkEyXkFqcGdeQXVyMjMxOTE0ODA@._V1_UX182_CR0,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BMTQ2MzE2NTk0NF5BMl5BanBnXkFtZTgwOTM3NTk1MjE@._V1_UX182_CR0,0,182,268_AL_.jpg" ,"https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_UX182_CR0,0,182,268_AL_.jpg"]
 const ActionMovieYear = [ "26 March 2021","26 September 2014", "18 July 2008","24 October 2014"]
+const ActionMovieRating = ["10","9","8","7"]
 const ActionMovieCast = [{Director:" Ilya Naishuller" ,Writer :"Derek Kolstad", Stars:"Bob Odenkirk, Aleksey Serebryakov, Connie Nielsen" },
 {Director:"Antoine Fuqua" ,Writer :"Richard Wenk, Michael Sloan", Stars:"Denzel Washington, Marton Csokas, Chloë Grace Moretz"},
 {Director:"Christopher Nolan" ,Writer :" Jonathan Nolan (screenplay), Christopher Nolan (screenplay)", Stars:"Christian Bale, Heath Ledger, Aaron Eckhart "},
@@ -48,9 +49,13 @@ for (let i=0; i<ActionMovieName.length;i++){
     $("#ActionMovie"+i).append(`<div class=movieDivCH2 id='actionCH${i}' ></div>`)
     $("#actionCH"+i).append(`<h1>${ActionMovieName[i]}</h1>`)
     $("#actionCH"+i).append(`<h2>${ActionMovieYear[i]}</h2>`)
+
+    localStorage.setItem(ActionMovieName[i],ActionMovieRating[i])
+    $("#actionCH"+i).append(`<h4>Rating : ${localStorage.getItem(ActionMovieName[i])} From 10</h4>`)
+    
     $("#actionCH"+i).append(`<p>${ActionMovieBref[i]}</p>`)
     $("#actionCH"+i).append(`<div class='moreInfo'><button id='actionmore${i}'>More Info ...</button></div>`)
-
+    
 
 
     $("#actionmore"+i).on("click", () => {
@@ -135,4 +140,4 @@ const comedyMovieCast =[{Director:"Patrick Hughes",Writer:"Tom O'Connor",Stars:"
         });
     }
 
-    movieDiv
+    
